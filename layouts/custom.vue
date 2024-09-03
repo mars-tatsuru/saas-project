41935f81caefc11744c6dad881a25aa82d9c578d
tatsuru koike <t-koike@marsflag.com>
2024-09-03 22:38:56 +0900

    custom layout

---
+++ layouts/custom.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import AppHeader from "@/components/AppHeader.vue";
+import AppSidebar from "@/components/AppSidebar.vue";
+import { useStore } from "@/stores/index";
+
+const route = useRoute();
+const store = useStore();
+
+/************************
+ * header toggle
+ *************************/
+const main = ref<HTMLElement | null>(null);
+watch(
+  () => store.isSidebarOpen,
+  () => {
+    main.value?.classList.toggle("md:!w-[calc(100%-4rem)]");
+  },
+);
+</script>
+
+<template>
+  <div class="whole bg-white dark:bg-black">
+    <!-- Main -->
+    <div
+      class="ml-auto flex h-screen min-h-screen bg-slate-100 px-4 py-0 transition-all duration-300 md:p-4 dark:bg-slate-900"
+      ref="main"
+    >
+      <div class="hidden h-full w-full md:block dark:border-gray-700">
+        <slot />
+      </div>
+
+      <div class="flex min-h-screen items-center justify-center md:hidden">
+        <p class="text-slate-800 dark:text-white">
+          You can only use this app with PC.<br />
+          Please use a PC to access this app.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/login.vue
+++ pages/login.vue
@@ -6,6 +6,10 @@ const user = useSupabaseUser();
 // use router
 const router = useRouter();
 
+definePageMeta({
+  layout: "custom",
+});
+
 watchEffect(() => {
   if (user.value) {
     router.push("/");
@@ -24,11 +28,13 @@ const login = async (providerName: "google" | "github") => {
 </script>
 
 <template>
-  <div class="container relative z-10 h-full max-w-none gap-5">
+  <div
+    class="relative z-10 flex h-full max-w-none flex-col items-center justify-center gap-5"
+  >
     <h1 class="text-2xl font-semibold text-gray-800 dark:text-gray-100">
       Login
     </h1>
-    <div class="mt-4">
+    <div class="">
       <button
         class="inline-flex items-center rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         @click="login('google')"
