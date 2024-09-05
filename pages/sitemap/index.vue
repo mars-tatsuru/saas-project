9cb2e425c91f229307d55e50e8b649b8a8b9550b
tatsuru koike <t-koike@marsflag.com>
2024-09-05 20:44:19 +0900

    change pages directly

---
+++ pages/sitemap/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+definePageMeta({
+  title: "sitemap",
+});
+
+useHead({
+  title: "sitemap",
+});
+
+/************************************************
+ * Determine if the user is logged in
+ *************************************************/
+definePageMeta({
+  middleware: "auth",
+});
+</script>
+
+<template>
+  <div class="container relative z-10 h-full max-w-none gap-5">
+    <h1 class="text-2xl font-semibold text-gray-800 dark:text-gray-100">
+      site map
+    </h1>
+    <NuxtLink to="/sitemap/created-sitemap" class="text-blue-500">
+      Go created-sitemap page
+    </NuxtLink>
+  </div>
+</template>
+
+<style>
+/* import the necessary styles for Vue Flow to work */
+@import "@vue-flow/core/dist/style.css";
+
+/* import the default theme, this is optional but generally recommended */
+@import "@vue-flow/core/dist/theme-default.css";
+</style>
